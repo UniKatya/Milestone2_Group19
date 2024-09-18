bf92854100662d0c51723f55d15e9a4cd55627c1
Ella <141898616+ellyiian@users.noreply.github.com>
2024-09-19 09:21:38 +1000

    Update template_frame.py

--- code/template_frame.py
+++ code/template_frame.py
@@ -501,6 +501,7 @@ class MyFrame1 ( wx.Frame ):
 		# Connect Events
 		self.m_buttonSearch.Bind( wx.EVT_BUTTON, self.display_nutritional_info )
 		self.m_buttonBreakdown.Bind( wx.EVT_BUTTON, self.display_charts )
+		self.m_buttonRangeSearch.Bind( wx.EVT_BUTTON, self.filter_food_by_nutrition_range )
 		self.m_button7.Bind( wx.EVT_BUTTON, self.display_meal_plan )
 		self.m_button8.Bind( wx.EVT_BUTTON, self.remove_food_from_meal_plan )
 		self.m_button9.Bind( wx.EVT_BUTTON, self.display_food )
@@ -516,6 +517,9 @@ class MyFrame1 ( wx.Frame ):
 	def display_charts( self, event ):
 		event.Skip()
 
+	def filter_food_by_nutrition_range( self, event ):
+		event.Skip()
+
 	def display_meal_plan( self, event ):
 		event.Skip()
 
@@ -524,5 +528,3 @@ class MyFrame1 ( wx.Frame ):
 
 	def display_food( self, event ):
 		event.Skip()
-
-
