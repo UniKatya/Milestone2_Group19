f9099bd21846a4344b359eed42007829bfaa8a34
ekaterina kozub <ekaterina.kozub@griffithuni.edu.au>
2024-10-02 11:50:51 +1000

    v_4.8

--- code/test_all_functions.py
+++ code/test_all_functions.py
@@ -204,8 +204,13 @@ def test_remove_food_from_meal_plan_invalid(meal_plan):
     assert exc_info.type is KeyError
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 def test_get_number_rows_valid(cream_cheese_info):
     assert get_number_rows(pd.DataFrame([cream_cheese_info])) == 1
+=======
+def test_get_number_rows(data_table):
+    assert data_table.GetNumberRows() == 3
+>>>>>>> parent of b12ee5d (v_4.6)
 =======
 def test_get_number_rows(data_table):
     assert data_table.GetNumberRows() == 3
@@ -214,6 +219,7 @@ def test_get_number_rows(data_table):
 def test_get_number_cols(data_table):
     assert data_table.GetNumberCols() == 4
 
+<<<<<<< HEAD
 <<<<<<< HEAD
 def test_get_number_cols_valid(cream_cheese_info):
     assert get_number_cols(pd.DataFrame([cream_cheese_info])) == 34
@@ -237,12 +243,21 @@ def test_get_value_invalid(data_table):
     with pytest.raises(IndexError) as exc_info:
         data_table.GetValue(10, 10)
 >>>>>>> parent of b12ee5d (v_4.6)
+=======
+def test_get_value_valid(data_table):
+    assert data_table.GetValue(0, 0) == 'apple'
+
+def test_get_value_invalid(data_table):
+    with pytest.raises(IndexError) as exc_info:
+        data_table.GetValue(10, 10)
+>>>>>>> parent of b12ee5d (v_4.6)
     assert exc_info.type is IndexError
 
 def test_set_value_valid(data_table):
     data_table.SetValue(0, 0, 10)
     assert data_table.GetValue(0, 0) == 10
 
+<<<<<<< HEAD
 <<<<<<< HEAD
 def test_set_value_invalid(cream_cheese_info):
     with pytest.raises(IndexError) as exc_info:
@@ -272,6 +287,22 @@ def test_get_col_label_value_invalid(data_table):
 >>>>>>> parent of b12ee5d (v_4.6)
     assert exc_info.type is IndexError
 
+=======
+def test_set_value_invalid(data_table):
+    with pytest.raises(IndexError) as exc_info:
+        data_table.SetValue(10, 10, 100)
+    assert exc_info.type is IndexError
+
+def test_get_col_label_value_valid(data_table):
+    assert data_table.GetColLabelValue(0) == 'food'
+    assert data_table.GetColLabelValue(1) == 'Caloric Value'
+
+def test_get_col_label_value_invalid(data_table):
+    with pytest.raises(IndexError) as exc_info:
+        data_table.GetColLabelValue(10)
+    assert exc_info.type is IndexError
+
+>>>>>>> parent of b12ee5d (v_4.6)
 def test_get_attr_valid(data_table):
     assert data_table.GetAttr(3, 0, None).GetBackgroundColour() == EVEN_ROW_COLOUR
     assert not data_table.GetAttr(0, 0, None).HasBackgroundColour()
